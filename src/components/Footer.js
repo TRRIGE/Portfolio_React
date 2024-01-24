462ac7ac42925251ec3df8ffd3a9ba9c366ee4f1
Aryan Samarth <pratiksamarth29@gmail.com>
2024-01-24 16:31:39 +0530

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,82 @@
-import React from "react"
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import logo from '../images/logo.png';
 
 export default function Footer() {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        var success = document.getElementById("success");
+        e.preventDefault();
+
+        emailjs.sendForm('service_x3a1w4v', 'template_2jksl4l', form.current, 'j-OEvWU-o-jF8HUHt')
+            .then((result) => {
+                e.target.reset();
+                success.style.display = "block";
+            }, (error) => {
+                console.log(error);
+            });
+
+            setTimeout(() => {
+             success.style.display = "none";
+            },7000)
+    }
+
+    function checkForm(e) {
+        var nameValue = document.getElementById('user_name').value.trim();
+        var emailValue = document.getElementById('user_email').value.trim();
+        var messageValue = document.getElementById('message').value.trim();
+  
+        var submitButton = document.getElementById('formButton');
+  
+        if (nameValue !== '' && emailValue !== '' && messageValue !== '') {
+          submitButton.removeAttribute('disabled');
+        } else {
+          submitButton.setAttribute('disabled', 'disabled');
+        }
+      }
+
     return (
         <>
             <div className="container">
-            <div className="row section" id="contact">
-                    <div className="col">
-                        <h4 style={{ color: '#03b672' }} id="edit"> - Contact with me!</h4>
+                <div className="row section" id="contact">
+                    <div className="col-lg-6">
+                        <h4 id="edit">Connect with me!</h4>
                         <div className="footer">
                             <img src={logo} height={110} width={110} alt="logo" />
-                            <p><i className="bi bi-globe-americas"></i> Planet Earth</p>
-                            <p><i className="bi bi-telephone"></i>+91 9359062282</p>
+                            <p><i className="bi bi-globe-americas"></i>Planet Earth</p>
                             <p><i className="bi bi-envelope"></i>pratiksamarth29@gmail.com</p>
                             <div className="mt-4" id="icons">
                                 <a className="text-white fs-5 me-4" href="https://www.linkedin.com/in/pratik-samarth-ps/"><i className="bi bi-linkedin"></i></a>
                                 <a className="text-white fs-5 me-4" href="https://github.com/TRRIGE"><i className="bi bi-github"></i></a>
                                 <a className="text-white fs-5 me-4" href="https://twitter.com/PratikSamarth"><i className="bi bi-twitter-x"></i></a>
-                                <a className="text-white fs-5 " href="https://www.instagram.com/"><i className="bi bi-instagram"></i></a>
+                                <a className="text-white fs-5 " href="https://www.instagram.com/rn_samarth/?next=%2F"><i className="bi bi-instagram"></i></a>
                             </div>
                         </div>
                     </div>
+                    <div className="col-lg-6 mt-sm-5 mt-lg-0 responsiveFooter">
+                        <h4 style={{ color: '#3AA3E7' }}>___ Contact me</h4>
+                        <form ref={form} onSubmit={sendEmail}>
+                            <div className="mb-3">
+                                <input type="text" name="user_name" className="form-control text-white" id="user_name" aria-describedby="emailHelp" placeholder="your full name" style={{ backgroundColor: "#171f38" }} onInput={checkForm} />
+                            </div>
+                            <div className="mb-3">
+                                <input type="email" name="user_email" className="form-control text-white" id="user_email" placeholder="your email" style={{ backgroundColor: "#171f38" }} onInput={checkForm} />
+                            </div>
+                            <div className="mb-3">
+                                <textarea name="message" className="form-control text-white" placeholder="your message" id="message" style={{ height: "100px", backgroundColor: "#171f38" }} onInput={checkForm} ></textarea>
+                            </div>
+                            <div className="d-grid">
+                                <button id='formButton' type="submit" value="Send" className="btn text-white" style={{ borderColor: '#374C89'}} disabled>Send Message</button>
+                            </div>
+                            <div className='fw-bold text-center mt-2 lead' id="success">Your message sent succesfully!</div>
+                        </form>
+                    </div>
                 </div>
-                <div className="row mb-3 mt-sm-3" id="contacts">
+                <div className="row mb-3 mt-5" id="contacts">
                     <div className="col">
-                        <h6 className="d-flex justify-content-lg-end justify-content-sm-center">© Copyright 2024 - Developed by Pratik Samarth. All right reserved.</h6>
+                        <h6 className="d-flex justify-content-lg-start justify-content-sm-center">© Copyright 2024 - Developed by Pratik Samarth. All right reserved.</h6>
                     </div>
                 </div>
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
             <nav className="navbar navbar-expand-lg p-1 sticky-top">
                 <div className="container">
                     <a className="navbar-brand text-white fs-2" href="#"><img src={logo} height={80} width={70} alt="logo" /><span style={{ color: '#03b672' }}>P</span>ratik <span style={{ color: '#03b672' }}>S</span>amarth</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <button className="navbar-toggler" style={{backgroundColor:"#03b672",color:"transparent"}} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -26,10 +26,10 @@ export default function Header() {
                             </li>
                         </ul>
                         <div className="d-flex" id="icons">
-                            <a className="text-white me-3" href="https://www.linkedin.com/in/pratik-samarth-ps/" target="_blank"><i className="bi bi-linkedin"></i></a>
-                            <a className="text-white me-3" href="https://github.com/TRRIGE" target="_blank"><i className="bi bi-github"></i></a>
-                            <a className="text-white me-3" href="https://twitter.com/PratikSamarth" target="_blank"><i className="bi bi-twitter-x"></i></a>
-                            <a className="text-white" href="https://www.instagram.com/" target="_blank"><i className="bi bi-instagram"></i></a>
+                            <a className="text-white me-3" href="https://www.linkedin.com/in/pratik-samarth-ps/" target="_blank" rel="noreferrer"><i className="bi bi-linkedin"></i></a>
+                            <a className="text-white me-3" href="https://github.com/TRRIGE" target="_blank" rel="noreferrer"><i className="bi bi-github"></i></a>
+                            <a className="text-white me-3" href="https://twitter.com/PratikSamarth" target="_blank" rel="noreferrer"><i className="bi bi-twitter-x"></i></a>
+                            <a className="text-white" href="https://www.instagram.com/rn_samarth/?next=%2F" target="_blank" rel="noreferrer"><i className="bi bi-instagram"></i></a>
                         </div>
                     </div>
                 </div>
