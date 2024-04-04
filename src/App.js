d6f07ace0161d528112d460322bb83988b0a2dd9
Aryan Samarth <pratiksamarth29@gmail.com>
2024-04-04 11:22:06 +0530

    Ninth Soft Commit 3

--- src/App.js
+++ src/App.js
@@ -38,7 +38,6 @@ function App() {
 
   return (
     <>
-
       {loading ?
         <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#020C1B' }}>
           <ClimbingBoxLoader
@@ -82,7 +81,7 @@ function App() {
               <a className="mb-4" href="https://github.com/TRRIGE" target="_blank" rel="noreferrer"><i className="bi bi-github"></i></a>
               <a className="mb-4" href="https://www.linkedin.com/in/pratik-samarth-ps/" target="_blank" rel="noreferrer"><i className="bi bi-linkedin"></i></a>
               <a className="mb-4" href="https://www.instagram.com/rn_samarth/?next=%2F" target="_blank" rel="noreferrer"><i className="bi bi-instagram"></i></a>
-              <a className="mb-4" href="https://dribbble.com/Pratik_Samarth/about" target="_blank" rel="noreferrer"><i class="bi bi-dribbble"></i></a>
+              <a className="mb-4" href="https://dribbble.com/Pratik_Samarth/about" target="_blank" rel="noreferrer"><i className="bi bi-dribbble"></i></a>
               <a style={{ marginBottom: '40px' }} href="https://twitter.com/PratikSamarth" target="_blank" rel="noreferrer"><i className="bi bi-twitter-x"></i></a>
             </motion.div>
             <motion.div
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -29,13 +29,13 @@ const Skills = () => {
                     initial={{ opacity: 0, y: 50 }}
                     animate={controls}
                     transition={{ duration: 0.9 }}
-                    className="card-group text-center">
+                    className="card-group text-center no-margin-on-mobile">
                     <div className="card" style={{ paddingTop: "70px" }}>
                         <i className="bi bi-layers mb-2" style={{ fontSize: "50px", color: "#64ffda" }}></i>
                         <div className="card-body mt-2">
                             <h4 className="card-title mb-3" style={{ color: '#ccd6f6' }}>Designer</h4>
                             <p className="card-text mb-4">I value simple content structure, clean designs, and thoughtful interactions.</p>
-                            <p className="card-text mb-4"><apan style={{ color: "#64ffda" }}>Things I enjoy designing: </apan><br /> UX, UI, Web, Logos</p>
+                            <p className="card-text mb-4"><span style={{ color: "#64ffda" }}>Things I enjoy designing: </span><br /> UX, UI, Web, Logos</p>
                             <p><span style={{ color: "#64ffda" }}>Design Tools: </span><br />Figma  <br />Pen & Paper  <br />Sketch  <br />Webflow
                             </p>
                         </div>
@@ -55,7 +55,7 @@ const Skills = () => {
                         <div className="card-body mt-2">
                             <h4 className="card-title mb-3" style={{ color: '#ccd6f6' }}>Backend Developer</h4>
                             <p className="card-text mb-4">I enjoy crafting and turning concepts into powerful functionality behind the scenes.</p>
-                            <p className="card-text mb-4"><span style={{ color: "#64ffda" }}>Languages I Use: </span><br /> Node, Express, JavaScript, PHP</p>
+                            <p className="card-text mb-4"><span style={{ color: "#64ffda" }}>Languages I Use: </span><br /> Node, Express, JavaScript, Python, PHP</p>
                             <p><span style={{ color: "#64ffda" }}>Dev Tools: </span><br />Firebase<br />Postman<br />MongoDB<br />MySQL<br />Render<br />VSCode<br />
                             </p>
                         </div>
